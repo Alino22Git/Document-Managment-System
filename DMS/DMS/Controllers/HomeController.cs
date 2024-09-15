e01685972c0b7de17ab7722044f47625e77a0644
Michael Reichenberger <michael.reichenberger@gmx.at>
2024-09-15 22:56:37 +0200

    Defaut Route
    Created default route that returns "HTTP 200 OK" and some hardcoded data when 'http://localhost:8081/' is called

---
+++ DMS/DMS/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DMS.Controllers
+{
+    [ApiController]
+    [Route("/")] // Basis-Route (http://localhost:8081/)
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetHardcodedData()
+        {
+            var hardcodedData = new
+            {
+                Message = "This is hardcoded data",
+                Timestamp = DateTime.UtcNow,
+                Status = 200
+            };
+
+            return Ok(hardcodedData);
+        }
+    }
+}
